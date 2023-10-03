030f89ea0423d512443918b880057a60720e25cc
FlameOWO <145371056+FlameOWO@users.noreply.github.com>
2023-10-03 20:57:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -200,7 +200,7 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/driver"
 	"github.com/wdvxdr1123/ZeroBot/message"
 
-	// webctrl "github.com/FloatTech/zbputils/control/web"
+	 webctrl "github.com/FloatTech/zbputils/control/web"
 
 	"github.com/FloatTech/ZeroBot-Plugin/kanban/banner"
 	// -----------------------以上为内置依赖，勿动------------------------ //
@@ -261,8 +261,8 @@ func init() {
 	 sus = append(sus, 2338206975)
 	// sus = append(sus, 87654321)
 
-	// 启用 webui
-	 go webctrl.RunGui(*g)
+	 启用 webui
+	// go webctrl.RunGui(*g)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
